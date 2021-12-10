d2984afb291138b8bc629a8fb9b9c5381dd6269c
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2021-12-10 15:45:29 -0300

    create initial configs and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './app/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'boilerplate',
+      username: 'boilerplate',
+      password: 'boilerplate',
+      entities: ['/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
